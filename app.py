2728acee61ae3874c27ee5a6dd29fde01ff29788
Sajal Singhal <95714873+SINGHALSAJAL@users.noreply.github.com>
2024-07-02 20:10:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template, render_template_string
+import csv
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def slang_meaning():
+    if request.method == "GET":
+        return render_template("input.html")
+    elif request.method == "POST":
+        input_slang = request.form['username'].upper()
+        output = "Slang not found"
+        with open('slangs.csv') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if row[1] == input_slang:
+                    output = row[2]
+                    break
+            
+        return render_template('output.html', output=output , input_slang=input_slang)
+
+if __name__ == '__main__':
+    app.debug = False
+    app.run()
+
